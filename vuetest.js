9fcade993c6bd81acb13ff7baf8fe089840984ca
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-08 21:22:41 +0900

    1-3 ボタンを押すと文字列を反転する

--- vuetest.js
+++ vuetest.js
@@ -6,5 +6,14 @@ new Vue({
         return{
             message: 'Messe'
         };
+    },
+    //メソッド
+    methods: {
+        reverseMessage: function(){
+            //this.message:このオブジェクトのmessage
+            //dataはいらないことに注意
+            //↓で文字列を反転できる
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 })
